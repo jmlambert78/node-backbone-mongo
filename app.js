2403afb6d4e34ec073a2a9e5fc9b82122f71137d
jmlambert78 <jmlambert78@gmail.com>
2019-03-04 11:52:58 +0100

    updated for scf

--- app.js
+++ app.js
@@ -32,14 +32,16 @@ var dbipaddr = process.env.MYMONGO_PORT_27017_TCP_ADDR|"127.0.0.1";
 var dbcreds ={"host":dbipaddr,"db":"","port":"27017","username":"test","password":"test"};
 if(process.env.VCAP_SERVICES){
   var services = JSON.parse(process.env.VCAP_SERVICES);
-  var dbcreds = services['mongodb'][0].credentials;
+  var dbcreds = services['azure-cosmosdb-mongo-account'][0].credentials;
+  dbcreds.db=dbcreds.username; // for azure cosmosdb
 }
 
 if(dbcreds){
-  console.log(dbcreds);
-  mongoose.connect(dbcreds.host, dbcreds.db, dbcreds.port, {user: dbcreds.username, pass: dbcreds.password});
+  var mongoDB = 'mongodb://'+dbcreds.username+':'+ encodeURIComponent(dbcreds.password)+"@"+dbcreds.host+':'+dbcreds.port+'/?ssl=true&replicaSet=globaldb';
+  mongoose.connect(mongoDB/*,{useNewUrlParser: true}*/);
 }else{
   mongoose.connect(dbipaddr, "todomvc", 27017);
+
 }
 
 http.createServer(app).listen(port);
